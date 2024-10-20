9368e8f0209e089738444ea748dfd69223b426c4
matej.fric <matej.fric@vsb.cz>
2024-10-20 21:25:42 +0200

    simplify fc ae

--- model/ae/fc.py
+++ model/ae/fc.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from .common import Decoder, Encoder
 
@@ -16,20 +17,16 @@ class FCEncoder(Encoder):
         ), f'Hidden layer size must be divisible by {divisibility_constraint}.'
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(image_size**2, hls)
-        self.relu1 = nn.ReLU()
         self.fc2 = nn.Linear(hls, hls // 2)
         self.fc3 = nn.Linear(hls // 2, hls // 4)
         self.fc4 = nn.Linear(hls // 4, hls // 8)
-        self.relu2 = nn.ReLU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.relu1(x)
-        x = self.fc2(x)
-        x = self.fc3(x)
-        x = self.fc4(x)
-        x = self.relu2(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
         return x
 
 
@@ -42,20 +39,16 @@ class FCDecoder(Decoder):
         ), f'Hidden layer size must be divisible by {divisibility_constraint}.'
         self.fc1 = nn.Linear(hls // 8, hls // 4)
         self.fc2 = nn.Linear(hls // 4, hls // 2)
-        self.relu1 = nn.ReLU()
         self.fc3 = nn.Linear(hls // 2, hls)
-        self.relu2 = nn.ReLU()
         self.fc4 = nn.Linear(hls, image_size**2)
-        self.unflatten = nn.Unflatten(1, torch.Size([1, image_size, image_size]))
         self.sigmoid = nn.Sigmoid()
+        self.unflatten = nn.Unflatten(1, torch.Size([1, image_size, image_size]))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = self.relu1(x)
-        x = self.fc3(x)
-        x = self.relu2(x)
-        x = self.fc4(x)
-        x = self.unflatten(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
         x = self.sigmoid(x)
+        x = self.unflatten(x)
         return x
