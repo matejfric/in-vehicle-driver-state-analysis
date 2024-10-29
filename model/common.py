19e1f22c20630e8c51f7d308acb59bd666baee08
matej.fric <matej.fric@vsb.cz>
2024-10-29 20:28:23 +0100

    add support for loading of anomaly annotations

--- model/common.py
+++ model/common.py
@@ -1,8 +1,9 @@
+from collections.abc import Iterator
+from dataclasses import dataclass
 from pathlib import Path
-from typing import TypedDict, Literal, TypeAlias
+from typing import Literal, TypeAlias, TypedDict
 
 from PIL import Image
-
 
 ModelStages: TypeAlias = Literal['train', 'valid', 'test']
 
@@ -27,3 +28,60 @@ def crop_driver_image_contains(image: Image.Image, image_path: Path) -> Image.Im
         # `2021_08_28_radovan_night_mask` different camera placement
         return image.crop((250, 0, 250 + image.size[1], image.size[1]))
     return image.crop((0, 0, image.size[1], image.size[1]))
+
+
+@dataclass
+class Anomaly:
+    start: int
+    end: int
+    labels: list[str]
+
+    def middle(self) -> int:
+        return (self.start + self.end) // 2
+
+
+class Anomalies:
+    def __init__(self, anomalies: list[Anomaly]) -> None:
+        self.anomalies = anomalies
+
+    def __len__(self) -> int:
+        return len(self.anomalies)
+
+    def __getitem__(self, index: int) -> Anomaly:
+        return self.anomalies[index]
+
+    def __repr__(self) -> str:
+        return f'Anomalies({self.anomalies})'
+
+    def __iter__(self) -> Iterator[Anomaly]:
+        return iter(self.anomalies)
+
+    @staticmethod
+    def from_file(path: str | Path) -> 'Anomalies':
+        """Read a text file and create an Anomalies instance."""
+        with open(path) as file:
+            data = file.readlines()
+
+        # Parse lines into a list of Anomaly dictionaries
+        parsed_data = []
+        for line in data:
+            if not line.strip():
+                continue
+            parts = line.split()
+            start = int(parts[0])
+            end = int(parts[1])
+            labels = parts[2:]
+            parsed_data.append(Anomaly(start=start, end=end, labels=labels))
+
+        return Anomalies(parsed_data)
+
+    def to_ground_truth(self, length: int) -> list[int]:
+        """Convert the anomalies to a ground truth list for binary classification.
+        Negative samples are labeled as 0, positive samples are labeled as 1.
+        """
+        ground_truth = [0] * length
+        for anomaly in self.anomalies:
+            ground_truth[anomaly.start : anomaly.end] = [1] * (
+                anomaly.end - anomaly.start
+            )
+        return ground_truth
