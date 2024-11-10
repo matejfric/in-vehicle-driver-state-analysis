06e9468dd8ffddaf94989a1a34e662580f499abc
matej.fric <matej.fric@vsb.cz>
2024-11-10 18:37:01 +0100

    add resize param to memory map conversion script

--- run_memory_map_conversion.sh
+++ run_memory_map_conversion.sh
@@ -12,12 +12,16 @@ declare -A DRIVER_MAP=(
 # Define the types to loop through
 TYPES=("normal" "anomal")
 
+# Set the resize parameter to the first argument,
+# or default to 128 if not provided.
+RESIZE=${1:-128}
+
 # Loop through each driver and type
 for DRIVER_DIR in "${!DRIVER_MAP[@]}"; do
     for TYPE in "${TYPES[@]}"; do
         # Construct the path and run the command
         PATH_TO_CONVERT="$HOME/source/driver-dataset/2024-10-28-driver-all-frames/${DRIVER_MAP[$DRIVER_DIR]}/$TYPE/depth"
         echo "Running conversion for DRIVER=$DRIVER_DIR, TYPE=$TYPE"
-        "$CONDA_PREFIX/bin/python3" run_memory_map_conversion.py --path "$PATH_TO_CONVERT" --resize 128
+        "$CONDA_PREFIX/bin/python3" run_memory_map_conversion.py --path "$PATH_TO_CONVERT" --resize "$RESIZE"
     done
 done
