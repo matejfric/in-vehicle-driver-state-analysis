1b6af76904f8e20cac0a488c3aab9686039f43d1
matej.fric <matej.fric@vsb.cz>
2025-03-01 17:48:57 +0100

    update augmentations

--- model/augmentation.py
+++ model/augmentation.py
@@ -6,7 +6,6 @@ from albumentations.pytorch.transforms import ToTensorV2
 def pre_transforms(image_size: int) -> list:
     return [
         albu.Resize(image_size, image_size, p=1, interpolation=cv2.INTER_NEAREST),
-        # albu.ToGray(p=1),
     ]
 
 
@@ -18,23 +17,25 @@ def hard_transforms() -> list:
     https://albumentations.ai/docs/api_reference/augmentations/dropout/coarse_dropout/
     https://albumentations.ai/docs/api_reference/full_reference/?h=horizon#albumentations.augmentations.geometric.transforms.HorizontalFlip
     """
-    result = [
+    return [
+        # albu.ToGray(p=1),
         albu.CoarseDropout(
-            mask_fill_value=0,
             num_holes_range=(1, 3),
             hole_height_range=(16, 32),
             hole_width_range=(16, 32),
-            p=0.3,
+            p=0.2,
         ),
-        albu.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.3),
-        albu.HorizontalFlip(p=0.3),
+        albu.RandomBrightnessContrast(p=0.2),
+        albu.HorizontalFlip(p=0.2),
+        albu.MotionBlur(p=0.1),
+        albu.GaussNoise(p=0.1, std_range=(0.1, 0.2)),
+        albu.RandomShadow(p=0.1),
+        albu.RandomSunFlare(p=0.1),
     ]
-
-    return result
 
 
 def post_transforms() -> list:
-    # Image is normalized in CorrosionModel
+    # Image is normalized in the model
     return [ToTensorV2(transpose_mask=True, p=1)]
 
 
