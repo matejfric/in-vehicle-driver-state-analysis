4bf8481b3f812dce959fe8ae194aa66322914341
matej.fric <matej.fric@vsb.cz>
2025-04-05 22:34:50 +0200

    update STAE runner

--- notebooks/stae/runner.py
+++ notebooks/stae/runner.py
@@ -2,18 +2,21 @@
 from datetime import datetime
 from pathlib import Path
 from pprint import pprint
+from typing import Literal
 
 import papermill as pm
 
 # %%
 job_dir_root = Path(f'jobs/{datetime.now().strftime("%Y-%m-%d-%H%M")}')
-drivers = ['geordi', 'poli', 'michal', 'dans', 'jakub']
-source_types = ['masks', 'depth']
+drivers = ['geordi']  # [1, 2, 3, 4, 5, 'geordi', 'poli', 'michal', 'dans', 'jakub']
+source_types = ['depth']  # ['masks', 'images', 'depth']
+dataset: Literal['mrl', 'dmd'] = 'mrl'
 pprint(
     dict(
         job_dir_root=job_dir_root,
         drivers=drivers,
         source_types=source_types,
+        dataset=dataset,
     )
 )
 
@@ -23,7 +26,11 @@ for driver in drivers:
         print(
             f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} processing: driver={driver}, source_type={source_type}'
         )
-        job_dir = job_dir_root / driver
+        job_dir = (
+            job_dir_root
+            / (driver if isinstance(driver, str) else f'driver_{driver}')
+            / source_type
+        )
         job_dir.parent.mkdir(parents=True, exist_ok=True)
         pm.execute_notebook(
             'train.ipynb',
@@ -31,6 +38,7 @@ for driver in drivers:
             parameters=dict(
                 driver=driver,
                 source_type=source_type,
+                dataset=dataset,
             ),
         )
 
