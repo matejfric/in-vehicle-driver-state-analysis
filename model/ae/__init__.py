3b1604a7550699ff478efea1c42c047efad209d9
matej.fric <matej.fric@vsb.cz>
2025-04-10 23:09:11 +0200

    remove unused code

--- model/ae/__init__.py
+++ model/ae/__init__.py
@@ -1,4 +1,2 @@
 from .common import evaluate_model_parallel, summarize_model  # noqa: F401
-from .conv import ConvDecoder, ConvEncoder  # noqa: F401
-from .fc import FCDecoder, FCEncoder  # noqa: F401
 from .temporal import LSTMDecoder, LSTMEncoder, TemporalAutoencoderModel  # noqa: F401
