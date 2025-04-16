220dfb2d1a2fec8dbeb8b6ce39cdfa5d0a43be71
matej.fric <matej.fric@vsb.cz>
2025-04-16 22:11:05 +0200

    update model download script

--- notebooks/example/download_models.py
+++ notebooks/example/download_models.py
@@ -8,10 +8,10 @@ from tqdm import tqdm
 
 # %%
 USER_NAME = 'matejfric'
-LOCAL_DIR = Path().cwd() / 'models2'
+LOCAL_DIR = Path().cwd() / 'models'
 
 seg_model_uri = 'mlflow-artifacts:/7631752f9ba344e58f10d59f972ac342/03d2f6c0e7794ff0b3e2c6b70af93a49/artifacts/model'
-ae_model_uri = 'mlflow-artifacts:/34294c9653fc48309a2302eb44b3be4b/175a88ca24ff4f7f8b265cf07a8d85e4/artifacts/model'
+ae_model_uri = 'mlflow-artifacts:/34294c9653fc48309a2302eb44b3be4b/f6aef4dd035340648ec48e696ca46c33/artifacts/model'
 
 seg_model_local_path = LOCAL_DIR / 'seg'
 ae_model_local_path = LOCAL_DIR / 'ae'
