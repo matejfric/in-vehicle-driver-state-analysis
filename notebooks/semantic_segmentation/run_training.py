88492405127b1417c9f0a0daec3a6466da177d58
matej.fric <matej.fric@vsb.cz>
2025-04-24 23:16:48 +0200

    retrain sem-seg models with updated dataset

--- notebooks/semantic_segmentation/run_training.py
+++ notebooks/semantic_segmentation/run_training.py
@@ -6,9 +6,19 @@ from pprint import pprint
 import papermill as pm
 
 # %%
-job_root = Path(f'jobs/{datetime.now().strftime("%Y-%m-%d")}-semantic-segmentation')
-encoders = ['mit_b1']  # ['mit_b0', 'efficientnet-b0', 'efficientnet-b1', 'resnet18']
-decoders = ['unet']  # , 'unetplusplus']
+job_root = Path(
+    f'jobs/{datetime.now().strftime("%Y-%m-%d-%H%M")}-semantic-segmentation'
+)
+encoders = [
+    'efficientnet-b0',
+    'efficientnet-b1',
+    'efficientnet-b2',
+    'efficientnet-b3',
+    'resnet18',
+    'mit_b0',
+    'mit_b1',
+]
+decoders = ['unet', 'unetplusplus']
 pprint(
     dict(
         job_root=job_root,
@@ -18,16 +28,19 @@ pprint(
 )
 
 # %%
+job_root.mkdir(parents=True, exist_ok=True)
 for encoder in encoders:
     for decoder in decoders:
+        if encoder.startswith('mit') and decoder == 'unetplusplus':
+            # Skip this combination as it is not supported.
+            continue
         print(
             f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} processing: encoder={encoder} | decoder={decoder}'
         )
-        job_dir = job_root / f'{encoder}-{decoder}'
-        job_dir.mkdir(parents=True, exist_ok=True)
+        job_path = job_root / f'{encoder}-{decoder}'
         pm.execute_notebook(
             'train.ipynb',
-            f'{str(job_dir)}.ipynb',
+            f'{str(job_path)}.ipynb',
             parameters=dict(
                 encoder=encoder,
                 decoder=decoder,
