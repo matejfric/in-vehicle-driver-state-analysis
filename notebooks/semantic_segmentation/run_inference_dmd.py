875143b92928f864e46e4b42ae36cb389f855459
matej.fric <matej.fric@vsb.cz>
2025-04-27 23:40:51 +0200

    implement sem seg inference for IR images

--- notebooks/semantic_segmentation/run_inference_dmd.py
+++ notebooks/semantic_segmentation/run_inference_dmd.py
@@ -6,47 +6,32 @@ from pprint import pprint
 import papermill as pm
 import tqdm
 
-from model.dmd import CATEGORIES, ROOT
+from model.dmd import ROOT, get_all_sessions
 
 # %%
-session_dirs: list[Path] = [x for x in ROOT.glob('*') if x.is_dir()]
-session_subdirs: list[Path] = [
-    ses_dir / class_dir for ses_dir in session_dirs for class_dir in CATEGORIES
-]
-all_subdirs: list[list[Path]] = [
-    list(subdir.glob('*')) for subdir in session_subdirs if subdir.is_dir()
-]
-all_dirs = sorted(
-    [
-        dir
-        for subdirs in all_subdirs
-        for dir in subdirs
-        if (dir / 'rgb').is_dir() and len(list((dir / 'rgb').glob('*.jpg'))) > 0
-    ]
-)
+sessions = get_all_sessions()
+all_dirs = [ROOT / s for s in sessions if (ROOT / s).is_dir()]
 job_root = Path(f'jobs/dmd/{datetime.now().strftime("%Y-%m-%d")}')
+job_root.mkdir(parents=True, exist_ok=True)
 
 print(f'Job root: {job_root}')
-print(f'Found {len(all_dirs)} sequences. Example:')
-pprint(all_dirs[0:5])
+print(f'Found {len(all_dirs)} sessions. Example:')
+pprint(all_dirs)
 
 # %%
 for dir in (pbar := tqdm.tqdm(all_dirs)):
-    sequence = dir.name
-    class_name = dir.parent.name
-    session = dir.parent.parent.name
-    job_dir = job_root / session / class_name / sequence
-    pbar.set_postfix_str(f'{session}/{class_name}/{sequence}')
-
-    job_dir.parent.mkdir(parents=True, exist_ok=True)
+    session_name = dir.name.split(';')[0]
+    job_dir = job_root / session_name
+    pbar.set_postfix_str(session_name)
     pm.execute_notebook(
         'job_batch_inference.ipynb',
         f'{str(job_dir)}.ipynb',
         parameters=dict(
-            model_name='pytorch-2025-03-23-170148-unetplusplus-efficientnet-b1',
-            model_version='champion',
-            input_dir=str(dir / 'rgb'),
+            model_name='pytorch-sem-seg',
+            model_version=26,
+            input_dir=str(dir),
             dataset='dmd',
+            source_type='ir',
         ),
     )
 
